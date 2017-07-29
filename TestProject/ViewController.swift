64ba40c56935a8caaa77650c3644438e3677e665
admin <admin@mac-mini-admin.local>
2017-07-29 12:24:43 +0300

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by admin on 29.07.17.
+//  Copyright © 2017 andru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Меняем фон на жёлтый
+        self.view.backgroundColor = UIColor.green // опа
+    _ = "йцуйцуйцуйцуйц \r аауцпукп"
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
